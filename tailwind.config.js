24def0e8b63acacba25f3ce1bfc4ea7e16ff63dd
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-08-20 10:33:36 +0800

    finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,13 @@ export default {
     theme: {
         extend: {
             colors: {
-
+                c_Dark_cyan: "hsl(185, 75%, 39%)",
+                c_Very_dark_desaturated_blue: "hsl(229, 23%, 23%)",
+                c_Dark_grayish_blue: "hsl(227, 10%, 46%)",
+                c_Dark_gray: "hsl(0, 0%, 59%)",
             },
             fontFamily: {
-
+                kumbh: ["Kumbh Sans", "sans-serif"]
             }
         },
     },
